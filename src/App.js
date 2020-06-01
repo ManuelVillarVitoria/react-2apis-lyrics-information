7580b9c836bcb95640d0ca67779b33a6bf321363
manu <manu.villar.vitoria@gmail.com>
2020-06-01 14:34:49 +0200

    obtaining the artist's information since another API

--- src/App.js
+++ src/App.js
@@ -9,24 +9,37 @@ function App() {
 
   const [busquedaLetra, guardarBusquedaLetra] = useState({});
   const [letra, guardarLetra] = useState('');
+  const [info, guardarInfo] = useState({});
 
   useEffect(() => {
+
     if(Object.keys(busquedaLetra).length === 0) return;
     //console.log('no se ejecuta');
 
     const consultarApiLetra = async () => {
+  
       const {artista, cancion} = busquedaLetra;
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+      
+      //Con Promise.all consultamos las APIS a la vez y no esperamos que se termine de ejecutar
+      //una para que empieza a ejecutarse la otra. De este modo, ganamos en performance.
+      const [letra, informacion] = await Promise.all([
+        axios(url),
+        axios(url2)
+      ])
 
-      const res = await axios(url);
-
-      guardarLetra(res.data.lyrics);
-
+      //console.log(letra.data.lyrics);
+      //console.log(informacion.data);
+      guardarLetra(letra.data.lyrics);
+      guardarInfo(informacion.data.artists[0]);
     }
+
     consultarApiLetra();
+
   }, [busquedaLetra]);
 
-  
+
   return (
     <Fragment>
       <Formulario 
